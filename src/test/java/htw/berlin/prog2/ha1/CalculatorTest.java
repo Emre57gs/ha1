3a0938a885b27a76edba7fabd2b93698ea99aa43
Emre57gs <Emre.Bas@Student.HTW-Berlin.de>
2025-10-15 14:33:45 +0200

    Erste Teilaufgabe gemacht, eine Subtraktion mit zwei positiven Zahlen als Test erstellt.

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalculatorTest {
 
     @Test
-    @DisplayName("should display result after adding two positive multi-digit numbers")
+    @DisplayName("should display result after adding two positive multi-digit numbers") // Addition von zwei positiven Zahlen
     void testPositiveAddition() {
         Calculator calc = new Calculator();
 
@@ -27,7 +27,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display result after getting the square root of two")
+    @DisplayName("should display result after getting the square root of two") // Wurzel aus zwei berechnen können
     void testSquareRoot() {
         Calculator calc = new Calculator();
 
@@ -41,7 +41,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display error when dividing by zero")
+    @DisplayName("should display error when dividing by zero") // wenn man durch 0 dividieren sollte ein "Error" erscheinen
     void testDivisionByZero() {
         Calculator calc = new Calculator();
 
@@ -57,7 +57,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display error when drawing the square root of a negative number")
+    @DisplayName("should display error when drawing the square root of a negative number") // Beim Wurzeln ziehen einer negativen Zahl sollte ein "Error" erscheinen
     void testSquareRootOfNegative() {
         Calculator calc = new Calculator();
 
@@ -72,20 +72,38 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should not allow multiple decimal dots")
+    @DisplayName("should not allow multiple decimal dots") // sollte nicht mehrere Dezimalzahlen zulassen
     void testMultipleDecimalDots() {
         Calculator calc = new Calculator();
 
         calc.pressDigitKey(1);
         calc.pressDotKey();
         calc.pressDigitKey(7);
-        calc.pressDotKey();
+        calc.pressDotKey(); // das wird hier bewusst nicht beachtet
         calc.pressDigitKey(8);
 
         String expected = "1.78";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Kein Ahnung") //Subtraktion von zwei positven Zahlen
+    void testPositiveSubtraktion() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(4);
+        calc.pressEqualsKey();
+        String expected = "1";
+
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+
     }
 
 
